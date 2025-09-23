f1e5b61ccc270d1471d42ded2a773ffb6a38f67a
rmzvr <rmzvr@proton.me>
2025-09-23 18:01:08 +0200

    feat(ex00): implement basic ScalarConverter class structure

---
+++ ex00/includes/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#ifndef SCALAR_CONVERTER_HPP
+# define SCALAR_CONVERTER_HPP
+
+# include <iostream>
+
+class ScalarConverter
+{
+	private:
+		
+	public:
+		ScalarConverter();
+		~ScalarConverter();
+};
+
+#endif
---
+++ ex00/sources/ScalarConverter.cpp
@@ -0,0 +1,15 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{
+	#ifdef DEBUG
+		std::cout << "ScalarConverter default constructor called" << std::endl;
+	#endif
+}
+
+ScalarConverter::~ScalarConverter()
+{
+	#ifdef DEBUG
+		std::cout << "ScalarConverter destructor called" << std::endl;
+	#endif
+}
