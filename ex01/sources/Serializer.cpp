9fcac49ff6ef0e5a6cc2d28e775bbb19f80d9830
rmzvr <rmzvr@proton.me>
2025-09-30 11:47:19 +0200

    feat(ex01): update serialize and deserialize methods to return proper values

--- ex01/sources/Serializer.cpp
+++ ex01/sources/Serializer.cpp
@@ -2,10 +2,10 @@
 
 uintptr_t Serializer::serialize( Data *ptr )
 {
-	return uintptr_t();
+	return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data *Serializer::deserialize( uintptr_t raw )
 {
-	return nullptr;
+	return reinterpret_cast<Data *>(raw);
 }
