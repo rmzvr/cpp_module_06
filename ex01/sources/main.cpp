eb32aa65de8b2cebea17225c376e88a07a3be196
rmzvr <rmzvr@proton.me>
2025-09-30 11:47:27 +0200

    feat(ex01): enhance main function to display data pointer details and serialization results

--- ex01/sources/main.cpp
+++ ex01/sources/main.cpp
@@ -3,9 +3,13 @@
 int	main( void )
 {
 	Data	data;
-	Data *	p_data = &data;
-	std::cout << data.a << std::endl;
-	uintptr_t p_ui = Serializer::serialize(p_data);
-	Serializer::deserialize(p_ui);
+	Data *	p1_data = &data;
+	std::cout << "data.a => " << p1_data->a << ", data address => " << p1_data << ", p1_data address => " << &p1_data << std::endl;
+	uintptr_t p_ui = Serializer::serialize(p1_data);
+	std::cout << "integer representation of data pointer => " << p_ui << std::endl;
+	Data *	p2_data = Serializer::deserialize(p_ui);
+	p2_data->a = 2;
+	std::cout << "data.a => " << p1_data->a << ", data address => " << p1_data << ", p1_data address => " << &p1_data << std::endl;
+	std::cout << "data.a => " << p2_data->a << ", data address => " << p2_data << ", p2_data address => " << &p2_data << std::endl;
 	return 0;
 }
